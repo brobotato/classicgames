76552e6a62abb6848632dbf4546cf1d32adf7ca2
brobotato <brianxu01@gmail.com>
2018-05-22 12:16:27 -0700

    add bullets and bullet collision with asteroids

--- games/asteroids.py
+++ games/asteroids.py
@@ -17,25 +17,27 @@ class Ship(pygame.sprite.Sprite):
         self.angle = 0
         self.acceleration = 0
         self.warp = 0
+        self.firing = 0
+        self.bullets = []
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, angle):
         pygame.sprite.Sprite.__init__(self)
-        self.image = GameEngine.sprite_dict['ship']
+        self.image = GameEngine.sprite_dict['bullet']
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
-        self.angle = 0
+        self.angle = angle
 
 
 class Asteroid(pygame.sprite.Sprite):
-    def __init__(self, x, y, size):
+    def __init__(self, x, y, angle, size):
         pygame.sprite.Sprite.__init__(self)
-        self.image = GameEngine.sprite_dict['ship']
+        self.image = GameEngine.sprite_dict['asteroid']
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
+        self.angle = angle
         self.size = size
-        self.angle = 0
 
 
 class Asteroids(GameState):
@@ -43,6 +45,8 @@ class Asteroids(GameState):
     def __init__(self):
         super(Asteroids, self).__init__()
         self.ship = Ship(GameEngine.display_width / 2, GameEngine.display_height / 2)
+        self.asteroids = []
+        self.time = 0
 
     def exit(self):
         pass
@@ -64,6 +68,9 @@ class Asteroids(GameState):
             self.ship.angle = (self.ship.angle - 5) % 360
         if keypresses[pygame.K_UP] != 0:
             self.ship.acceleration = 6
+        if self.ship.firing == 0 and keypresses[pygame.K_SPACE] != 0:
+            self.ship.firing = 2
+            self.ship.bullets.append(Bullet(self.ship.rect.x+15,self.ship.rect.y+17,self.ship.angle))
         if self.ship.warp == 0 and (keypresses[pygame.K_LSHIFT] != 0 or keypresses[pygame.K_RSHIFT] != 0):
             self.ship.warp = 20
             self.ship.rect.x = random.randint(0, GameEngine.display_width)
@@ -74,6 +81,7 @@ class Asteroids(GameState):
         self.ship.rect.y -= self.ship.acceleration * math.cos(math.radians(self.ship.angle))
         self.ship.acceleration = max(self.ship.acceleration - .1, 0)
         self.ship.warp = max(self.ship.warp - .5, 0)
+        self.ship.firing = max(self.ship.firing - .5, 0)
         if self.ship.rect.x > GameEngine.display_width:
             self.ship.rect.x = 0
         if self.ship.rect.x < 0:
@@ -82,10 +90,48 @@ class Asteroids(GameState):
             self.ship.rect.y = 0
         if self.ship.rect.y < 0:
             self.ship.rect.y = GameEngine.display_height
+        if random.randint(0,100) > 95:
+            if random.random() > 0.5:
+                if random.random() > 0.5:
+                    self.asteroids.append(Asteroid(0,random.randint(0,GameEngine.display_height),random.randint(0,360),3))
+                else:
+                    self.asteroids.append(Asteroid(GameEngine.display_width,random.randint(0,GameEngine.display_height),random.randint(0,360),3))
+            else:
+                if random.random() > 0.5:
+                    self.asteroids.append(Asteroid(random.randint(0,GameEngine.display_width),0,random.randint(0,360),3))
+                else:
+                    self.asteroids.append(Asteroid(random.randint(0,GameEngine.display_width),GameEngine.display_height,random.randint(0,360),3))
+        for bullet in self.ship.bullets:
+            bullet.rect.x -= 9 * math.sin(math.radians(bullet.angle))
+            bullet.rect.y -= 9 * math.cos(math.radians(bullet.angle))
+            if bullet.rect.x < 0 or bullet.rect.x > GameEngine.display_width:
+                self.ship.bullets.remove(bullet)
+            elif bullet.rect.y < 0 or bullet.rect.y > GameEngine.display_height:
+                self.ship.bullets.remove(bullet)
+            else:
+                for asteroid in self.asteroids:
+                    if abs(bullet.rect.x-asteroid.rect.x) < 12.5*asteroid.size and abs(bullet.rect.y-asteroid.rect.y) < 12.5*asteroid.size:
+                        self.asteroids.remove(asteroid)
+                        self.ship.bullets.remove(bullet)
+        for asteroid in self.asteroids:
+            asteroid.rect.x -= 6/asteroid.size * math.sin(math.radians(asteroid.angle))
+            asteroid.rect.y -= 6/asteroid.size * math.cos(math.radians(asteroid.angle))
+            if asteroid.rect.x > GameEngine.display_width:
+                asteroid.rect.x = 0
+            if asteroid.rect.x < 0:
+                asteroid.rect.x = GameEngine.display_width
+            if asteroid.rect.y > GameEngine.display_height:
+                asteroid.rect.y = 0
+            if asteroid.rect.y < 0:
+                asteroid.rect.y = GameEngine.display_height
         super().update(50)
 
     def draw(self):
         GameEngine.game_display.fill((0, 0, 0))
         GameEngine.game_display.blit(utils.rot_center(self.ship.image, self.ship.angle),
                                      (self.ship.rect.x, self.ship.rect.y))
+        for bullet in self.ship.bullets:
+            GameEngine.game_display.blit(bullet.image,(bullet.rect.x,bullet.rect.y))
+        for asteroid in self.asteroids:
+            GameEngine.game_display.blit(pygame.transform.scale(asteroid.image,(25*asteroid.size,25*asteroid.size)),(asteroid.rect.x,asteroid.rect.y))
         super().draw()
--- utils.py
+++ utils.py
@@ -8,9 +8,7 @@ def create_image(sprite_name):
 
 
 def rot_center(image, angle):
-    orig_rect = image.get_rect()
-    rot_image = pygame.transform.rotate(image, angle)
-    rot_rect = orig_rect.copy()
-    rot_rect.center = rot_image.get_rect().center
-    rot_image = rot_image.subsurface(rot_rect).copy()
-    return rot_image
+    loc = image.get_rect().center 
+    rot_sprite = pygame.transform.rotate(image, angle)
+    rot_sprite.get_rect().center = loc
+    return rot_sprite
